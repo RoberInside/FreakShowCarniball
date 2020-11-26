f930d77e14b74193a469d4a838c29a938b45383d
RoberInside <roberto.alcazar@campusfp.es>
2020-11-26 10:37:20 +0100

    BallController
    Script terminado y añadidos limites para que no se salga de la pantalla.

---
+++ FreakShowCarnival/Assets/Scripts/BallController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{  
+    public float speed;
+
+    public Rigidbody2D ballRb;
+    public GameObject ballGO;
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRb = GetComponent<Rigidbody2D>();
+        ballGO = GameObject.Find("Atlas_pelota");
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        
+        Move(moveH);        
+        
+    }
+    private void Move(float axis)     //esto para puebas con teclado luego se cambia por el input de android
+    {
+        Vector2 movement = new Vector3(axis, 0.0f);
+        ballRb.AddForce( movement * speed);
+    }
+}
