94648e597f0c28bea47f0d71cb2f9556640ed93b
RoberInside <roberto.alcazar@campusfp.es>
2020-11-30 21:06:21 +0100

    prueba gyroscopio
    Primera prueba de giroscopio de la pelota.

--- FreakShowCarnival/Assets/Scripts/BallController.cs
+++ FreakShowCarnival/Assets/Scripts/BallController.cs
@@ -11,27 +11,40 @@ public class BallController : MonoBehaviour
     public GameManager gameManagerSC;
     public Camera cam;
 
+    public Gyroscope m_Gyro;
+    
+    public bool gyroActive;
     // Start is called before the first frame update
     void Start()
     {
+        Input.backButtonLeavesApp = true;
         gameManagerSC = FindObjectOfType<GameManager>();
         ballRb = GetComponent<Rigidbody2D>();
         ballGO = GameObject.Find("Atlas_pelota");
-        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();        
+       EnableGyro();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveH = Input.GetAxis("Horizontal");
+        float moveH = Input.GetAxis("Horizontal");        
+        Move(m_Gyro, moveH);
         
-        Move(moveH);        
         
     }
-    private void Move(float axis)     //esto para puebas con teclado luego se cambia por el input de android
+    private void Move(Gyroscope g, float axis)     //esto para puebas con teclado luego se cambia por el input de android
     {
-        Vector2 movement = new Vector3(axis, 0.0f);
-        ballRb.AddForce( movement * speed);
+        Vector2 movement;
+        if (gyroActive)                        //girsocopio
+        {
+            movement = new Vector3(g.rotationRate.y , 0.0f);
+        }
+        else
+            movement = new Vector3(axis, 0.0f);
+        
+        ballRb.AddForce(movement * speed);
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -44,4 +57,11 @@ public class BallController : MonoBehaviour
         }
 
     }
+    public void EnableGyro()
+    {
+        m_Gyro = Input.gyro;                                                //girsocopio
+        m_Gyro.enabled = gyroActive;
+    }
+    
+
 }
