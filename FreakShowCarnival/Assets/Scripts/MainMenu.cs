92d026c470b6505398d87533bfafd16cbfef83cf
cecilpz <cecilia.lopez@campusfp.es>
2020-11-30 22:52:35 +0100

    menus terminados
    falta terminar el de opciones

---
+++ FreakShowCarnival/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Animations;
+
+public class MainMenu : MonoBehaviour
+{
+    public MainMenu menu;
+    public Animator cortDcha;
+    public Animator cortIzda;
+    public Animator cartel;
+    public bool activeMenu;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ActiveMenu()
+    {
+        cortDcha = GetComponent<Animator>();
+        menu = GetComponent<MainMenu>();
+        if (true)
+        {
+
+        }
+    }
+}
