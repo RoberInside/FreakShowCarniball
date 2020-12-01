2d556a915184578ed3b34d74f0e7675f066713a2
cecilopez <62647508+cecilopez@users.noreply.github.com>
2020-12-01 13:56:46 +0100

    cambios leves mneu
    no he hecho una mierda pero bueno

--- FreakShowCarnival/Assets/Scripts/MainMenu.cs
+++ FreakShowCarnival/Assets/Scripts/MainMenu.cs
@@ -11,24 +11,30 @@ public class MainMenu : MonoBehaviour
     public Animator cortDcha;
     public Animator cortIzda;
     public Animator cartel;
+    public AnimationClip cartelClip;
     public bool activeMenu;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  
     }
-
+    private void Start()
+    {
+        cartel = GameObject.Find("atlas_cartel").GetComponent<Animator>();
+        
+        cortDcha = GetComponent<Animator>();
+        //ActiveMenu();
+    }
     public void QuitGame()
     {
         Application.Quit();
     }
+    
+    //public void ()
+    //{
+    //    menu = GetComponent<MainMenu>();
+    //    if ()
+    //    {
 
-    public void ActiveMenu()
-    {
-        cortDcha = GetComponent<Animator>();
-        menu = GetComponent<MainMenu>();
-        if (true)
-        {
-
-        }
-    }
+    //    }
+    //}
 }
