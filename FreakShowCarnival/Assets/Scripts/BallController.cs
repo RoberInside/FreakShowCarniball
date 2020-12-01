96243ca257520690974b5e27afb903188f64d822
RoberInside <roberto.alcazar@campusfp.es>
2020-12-01 12:32:55 +0100

    Gyroscopio "terminado"
    gyroscopio y bordes añadidos, falta que se autoescalen los bordes

--- FreakShowCarnival/Assets/Scripts/BallController.cs
+++ FreakShowCarnival/Assets/Scripts/BallController.cs
@@ -11,40 +11,43 @@ public class BallController : MonoBehaviour
     public GameManager gameManagerSC;
     public Camera cam;
 
-    public Gyroscope m_Gyro;
-    
     public bool gyroActive;
+    public Gyroscope m_Gyro;    
     // Start is called before the first frame update
     void Start()
     {
         Input.backButtonLeavesApp = true;
         gameManagerSC = FindObjectOfType<GameManager>();
         ballRb = GetComponent<Rigidbody2D>();
-        ballGO = GameObject.Find("Atlas_pelota");
+        ballGO = GameObject.Find("Pelota");
         cam = GameObject.Find("Main Camera").GetComponent<Camera>();        
-       EnableGyro();
+        EnableGyro();                                                     //se activa el girocopio si el dispositivo tiene
 
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveH = Input.GetAxis("Horizontal");        
-        Move(m_Gyro, moveH);
-        
-        
+        float moveH = Input.GetAxis("Horizontal");                  //prueba niveles
+        Move(m_Gyro, moveH);                
     }
-    private void Move(Gyroscope g, float axis)     //esto para puebas con teclado luego se cambia por el input de android
+    private void Move(Gyroscope g, float axis)     
     {
         Vector2 movement;
-        if (gyroActive)                        //girsocopio
+        float impulso;
+        if (gyroActive)                                              //girsocopio si esta activado se usa sino no
         {
-            movement = new Vector3(g.rotationRate.y , 0.0f);
+            movement = new Vector2(g.rotationRate.z ,0.0f );       //la direccion en la que se mueve en la escena dada por la orientacion del giroscopio
+            impulso = g.gravity.y;                                   //se toma la gravedad del giroscopio como la magnitud del impulso de la pelota
         }
-        else
+        else                                                        //pruebas de nivel con teclado
+        {
             movement = new Vector3(axis, 0.0f);
-        
-        ballRb.AddForce(movement * speed);
+            impulso = speed;
+            ballRb.AddForce(movement * speed);
+        }
+        ballRb.AddForce(movement * impulso);                        //se añade la fueza correspondiente 
+
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -53,14 +56,18 @@ public class BallController : MonoBehaviour
             other.gameObject.SetActive(false);
             gameManagerSC.puntuacion += other.gameObject.GetComponent<PickUp>().value;
             gameManagerSC.SetScoreText();
-
         }
 
     }
     public void EnableGyro()
     {
-        m_Gyro = Input.gyro;                                                //girsocopio
-        m_Gyro.enabled = gyroActive;
+        if (SystemInfo.supportsGyroscope)                           //si el dispositivo soporta giroscopio
+        {
+            m_Gyro = Input.gyro;
+            m_Gyro.enabled = gyroActive;
+        }
+        else
+            gyroActive = false;
     }
     
 
