5edf26098a55a1d648689bf3527a4b029090d980
cecilpz <cecilia.lopez@campusfp.es>
2020-12-05 15:22:34 +0100

    puto boton

---
+++ FreakShowCarnival/Assets/Scripts/SoundButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButton : MonoBehaviour
+{
+    public Button bttn;
+    public Sprite onbttn;
+    public Sprite offbttn;
+    public bool activeSound;
+    private void Start()
+    {
+        bttn = FindObjectOfType<Button>();
+        activeSound = true;
+    }
+    public void ChangeImage()
+    {
+        if (!activeSound)
+        {
+            bttn.image.overrideSprite = offbttn;
+            activeSound = true;
+        }
+        else
+        {
+            bttn.image.overrideSprite = onbttn;
+            activeSound = false;
+        }
+        AudioListener.pause = activeSound;
+    }
+}
